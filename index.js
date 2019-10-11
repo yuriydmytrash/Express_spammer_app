60409e9caff31d5b32d213e468037f16559b7583
dmytryy <dmytrash.yura@gmail.com>
2019-10-11 09:58:42 -0700

    fixed authRoutes

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 require('./services/passport');
-const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-authRoutes(app);
+require('./routes/authRoutes')(app);
 
 
 const PORT = process.env.PORT || 5000;
