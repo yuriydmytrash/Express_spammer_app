5675878b6bd2af6c641bdd72eb44d50c4f65d2ca
dmytryy <dmytrash.yura@gmail.com>
2019-10-13 15:38:28 -0700

    adding cookie session

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
-
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+    keys: [keys.cookieKey]
+  })
+);
 
 require('./routes/authRoutes')(app);
 
