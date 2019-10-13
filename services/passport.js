6bb46ed5a1531b7aa56ee811e9e6f3e8c8bfaf76
dmytryy <dmytrash.yura@gmail.com>
2019-10-13 14:14:35 -0700

    adding mongoose query with existing user

--- services/passport.js
+++ services/passport.js
@@ -14,7 +14,14 @@ passport.use(new GoogleStrategy(
     callbackURL: '/auth/google/callback'
   },
   (accessToken, refreshToken, profile, done) => {
-    new User({ googleId: profile.id }).save();
+    User.findOne({ googleId: profile.id })
+      .then((existingUser) => {
+        if (existingUser) {
+
+        } else {
+          new User({ googleId: profile.id }).save();
+        }
+      });
   }
 )
 );
