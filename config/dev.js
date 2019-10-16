38e04ed516d85296f5614daecead274db2cf1290
dmytryy <dmytrash.yura@gmail.com>
2019-10-15 21:48:20 -0700

    adding prod

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '109336735432-sfjatfkst4586senp0r1nhq0jui0mnbc.apps.googleusercontent.com',
+  googleClientSecret: 'bHchYoqVzGv-jXHHHC9GIHgJ',
+  mongoURI: 'mongodb+srv://yuriy:Million!993@spammer-o8dtm.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: 'dlkasjdsakjdsa'
+};
